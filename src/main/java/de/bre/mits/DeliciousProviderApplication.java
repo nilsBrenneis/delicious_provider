30bbab419a1de725753f33532c926bf1b8ea9f43
Nils <nilsstnurfuersam@gmail.com>
2017-06-27 13:42:21 +0200

    Initial commit

---
+++ src/main/java/de/bre/mits/DeliciousProviderApplication.java
@@ -0,0 +1,12 @@
+package de.bre.mits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciousProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciousProviderApplication.class, args);
+	}
+}
